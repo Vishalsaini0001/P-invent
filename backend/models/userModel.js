bf87e7a2a83810e4b44a520c03448f36272fc0d1
vishal <vishal.saini.99154@gmail.com>
2025-01-09 15:31:20 +0530

    user Model added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,14 +12,13 @@ const userSchema = mongoose.Schema(
       unique: true,
       trim: true,
       match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         "Please fill a valid email address",
       ],
     },
     password: {
       type: String,
-      required: true,
-      required: [true, "Please Enter valid password"],
+      required: [true, "Please Enter a Valid Password"],
       minLength: [6, "Password must be more than 6 letters"],
       maxLength: [12, "Password must be less than 12 letters"],
     },
@@ -27,7 +26,6 @@ const userSchema = mongoose.Schema(
       type: String,
       default:
         "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
-      required: [true, "Please upload a Profile Picture"],
     },
     phone: {
       type: String,
@@ -40,6 +38,7 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
