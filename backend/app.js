30a326d69b983227f57afab275fd2ae63f889f29
vishal <vishal.saini.99154@gmail.com>
2025-01-09 11:49:02 +0530

    chalk package

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
 const ConnectDb = require("./utils/db");
+const chalk = require('chalk');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
 
 const PORT = process.env.PORT || 3000;
 
 ConnectDb().then(() => {
   app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+    console.log(chalk.cyan("Server is running on port", PORT));
   });
 });
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,12 @@
+const chalk = require("chalk");
 const mongoose = require("mongoose");
 
 const ConnectDb = async ()=>{
     try {
        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDb Connected...");
+        console.log(chalk.bgCyan("MongoDb Connected..."));
     } catch (error) {
-        console.log("MongoDb connection failed!",error)
+        console.log(chalk.red("MongoDb connection failed!",error));
     }
 }
 
