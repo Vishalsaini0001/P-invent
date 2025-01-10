ed7df41a6a20d1e42bb126f13237c94dbf419bf9
vishal <vishal.saini.99154@gmail.com>
2025-01-10 12:49:31 +0530

    removing hash fun from handler

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,13 +1,13 @@
 const chalk = require("chalk");
 const mongoose = require("mongoose");
 
-const ConnectDb = async ()=>{
-    try {
-       await mongoose.connect(process.env.MONGODB_URI);
-        console.log(chalk.bgCyan("MongoDb Connected..."));
-    } catch (error) {
-        console.log(chalk.red("MongoDb connection failed!",error))
-    }
-}
+const ConnectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(chalk.bgCyan("MongoDb Connected..."));
+  } catch (error) {
+    console.log(chalk.red("MongoDb connection failed!", error));
+  }
+};
 
 module.exports = ConnectDb;
