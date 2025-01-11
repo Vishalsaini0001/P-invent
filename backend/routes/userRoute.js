99c534f19c84a3970166887c82095c5afcd70c16
vishal <vishal.saini.99154@gmail.com>
2025-01-11 16:14:10 +0530

    login auth

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 const authController = require("../controllers/authController")
 
 Router.route("/register").post(authController.register)
+Router.route("/login").post(authController.login)
 
 module.exports = Router;
