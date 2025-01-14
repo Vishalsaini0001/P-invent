3c6aadc9785e32de78ed874090e23df9258d2b86
vishal <vishal.saini.99154@gmail.com>
2025-01-14 21:38:45 +0530

    changepassword route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const Router = express.Router();
 const authController = require("../controllers/authController")
+const protect = require("../middleware/authmiddleware")
 
 Router.route("/register").post(authController.register)
 Router.route("/login").post(authController.login)
-Router.route("/logout").post(authController.logout)
+Router.route("/logout").get(authController.logout)
+Router.route("/getUser").get(protect, authController.getUser)
+Router.route("/loggedin").get(authController.loggedInStatus)
+Router.route("/updateuser").patch(protect,authController.updateUser)
+Router.route("/changepassword").patch(protect,authController.changepassword)
+Router.route("/forgotpassword").post(authController.forgotpassword)
 
 module.exports = Router;
