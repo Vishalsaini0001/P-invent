c0ffb785bc6a86aa1d5e240dbab957209469ea8d
vishal <vishal.saini.99154@gmail.com>
2025-01-15 21:22:47 +0530

    token model

---
+++ backend/models/tokenModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  expireAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+const token = mongoose.model("token", tokenSchema);
+
+module.exports = token;
