182903ff866afa4d65f1870cb9cb4a1e0bc2ab87
vishal <vishal.saini.99154@gmail.com>
2025-01-28 22:01:58 +0530

    redux toolkit store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
