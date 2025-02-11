7290d69a0137b00addb6055348501f07e4fb1db5
vishal <vishal.saini.99154@gmail.com>
2025-02-11 21:20:40 +0530

    server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
+import Sidebar from "./components/sidebar/Sidebar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Layout from "./components/layout/layout"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/resetpassword/:resetToken" element={<Reset />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Sidebar>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
