5217939b7f7acba649313e44ff92ef0e6cbc6f81
vishal <vishal.saini.99154@gmail.com>
2025-02-13 23:14:56 +0530

    header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+
+const Header = () => {
+
+ 
+  return (
+    <div className="--pad header">
+      <div className="--flex-between">
+        <h3>
+          <span className="--fw-thin">Welcome, </span>
+          <span className="--color-danger">xyz..</span>
+        </h3>
+        <button className="--btn --btn-danger">
+          Logout
+        </button>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
