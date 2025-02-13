61f59b9a937a308dcae704d289e4208d03d82c9c
vishal <vishal.saini.99154@gmail.com>
2025-02-13 23:15:19 +0530

    auth slice

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,16 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
+const name = JSON.parse(localStorage.getItem("name"));
 const initialState = {
-    isLoggedIn: false
-
-}
+  isLoggedIn: false,
+  name: name ? name : "",
+  user: {
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    photo: "",
+  },
+  userId: "",
+};
 
 const authSlice = createSlice({
   name: "auth ",
   initialState,
-  reducers: {}
-});
+  reducers: {
+    SET_LOGIN(state, action){
+      state.isLoggedIn = action.payload
+    },
+    SET_NAME(state, action){
+      localStorage.setItem("name", JSON.stringify(action.payload))
+      state.name = action.payload
+      state.email = action.email
+      state.phone = action.phone
+      state.bio = action.bio
+      state.photo = action.photo
+    },
+  },
+})
 
-export const {} = authSlice.actions
+export const {} = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
